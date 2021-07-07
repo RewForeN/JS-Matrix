03fcb4e2e408f650ffe9677b0d84c07cdf8b51de
Ruan <ruane.swanepoel@gmail.com>
2021-07-08 02:07:13 +1000

    Implemented matrix multiplication

--- src/matrix.js
+++ src/matrix.js
@@ -41,23 +41,36 @@ class Matrix {
 		return this.data.length;
 	}
 
-	at(rowIndex, colIndex = undefined) {
-		Assert.index(rowIndex, 'Matrix.at()');
+	// TODO: Add test
+	// TODO: Implement
+	inverse() {
+
+	}
+
+	at(row, col = undefined) {
+		Assert.index(row, 'Matrix.at()');
 		let index;
-		if (colIndex !== undefined) {
-			Assert.index(colIndex, 'Matrix.at()');
-			Assert.integerMax(rowIndex, this.rows, 'Matrix.at()');
-			Assert.integerMax(colIndex, this.cols, 'Matrix.at()');
-			index = this.indexFromRowCol(rowIndex, colIndex);
+		if (col !== undefined) {
+			Assert.index(col, 'Matrix.at()');
+			Assert.integerMax(row, this.rows, 'Matrix.at()');
+			Assert.integerMax(col, this.cols, 'Matrix.at()');
+			index = this.indexFromRowCol(row, col);
 		}
 		else {
-			Assert.integerMax(rowIndex, this.len(), 'Matrix.at()');
-			index = rowIndex;
+			Assert.integerMax(row, this.len(), 'Matrix.at()');
+			index = row;
 		}
 		return this.data[index];
 	}
 
 	// TODO: Add test
+	// TODO: Error checks
+	set(row, col, value) {
+		this.data[this.indexFromRowCol(row, col)] = value;
+	}
+
+	// TODO: Add test
+	// TODO: Implement single value addition
 	add(other) {
 		if (!Matrix.isMatrix(other)) throw new TypeError('Parameter must be a Matrix');
 		return this.data.map((val, index) => {
@@ -66,6 +79,7 @@ class Matrix {
 	}
 
 	// TODO: Add test
+	// TODO: Implement single value subtraction
 	sub(other) {
 		if (!Matrix.isMatrix(other)) throw new TypeError('Parameter must be a Matrix');
 		return this.data.map((val, index) => {
@@ -74,17 +88,30 @@ class Matrix {
 	}
 
 	// TODO: Add test
+	// TODO: Implement single value multiplication
 	mul(other) {
 		if (!Matrix.isMatrix(other)) throw new TypeError('Parameter must be a Matrix');
-		return this.data.map((val, index) => {
-			// TODO: Implement
-		});
+		if (this.cols !== other.rows) throw new RangeError('The given Matrix\'s rows dont match this Matrix\'s columns.');
+		let newMat = Matrix.zeros(this.rows, other.cols);
+		for (let i = 0; i < this.rows; i++) {
+			for (let j = 0; j < other.cols; j++) {
+				let val = 0;
+				for (let k = 0; k < this.cols; k++) {
+					val += this.at(i, k) * other.at(k, j);
+					// TODO: Single value add not implemented
+					// newMat.add(i, j, this.at(i, k) * other.at(k, j));
+				}
+				newMat.set(i, j, val);
+			}
+		}
+		return newMat;
 	}
 
 	// TODO: Add test
+	// TODO: Implement matrix division
+	// TODO: Implement single value division
 	div(other) {
 		if (!Matrix.isMatrix(other)) throw new TypeError('Parameter must be a Matrix');
-		// TODO: Implement
 	}
 
 	// TODO: Add test
