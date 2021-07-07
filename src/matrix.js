dd152ac2d469a4e937eaf0889499e61ede299e28
Ruan <ruane.swanepoel@gmail.com>
2021-07-07 23:30:06 +1000

    Added tests and fixed functions for Matrix

--- src/matrix.js
+++ src/matrix.js
@@ -1,7 +1,7 @@
 class Matrix {
 
 	constructor(rows, cols, data) {
-		ErrorCheckHelper.assertIndices({'rows': rows, 'cols': cols});
+		ErrorCheckHelper.assertIndices({ 'rows': rows, 'cols': cols });
 		if (!Array.isArray(data)) throw new TypeError('[data] should be an array');
 		if (rows * cols !== data.length) throw new RangeError('[rows] and [cols] should match the length of [data]');
 		this.rows = rows;
@@ -15,37 +15,43 @@ class Matrix {
 		return new Matrix(data.length, data[0].length, data.flat(2));
 	}
 
-	// TODO: Add test
 	static zeros(rows, cols) {
 		return Matrix.filled(rows, cols, 0);
 	}
 
-	// TODO: Add test
 	static ones(rows, cols) {
 		return Matrix.filled(rows, cols, 1);
 	}
 
-	// TODO: Add test
 	static filled(rows, cols, value) {
-		ErrorCheckHelper.assertIndices({'rows': rows, 'cols': cols});
-		return (new Array(rows)).fill(new Array(cols).fill(value));
+		ErrorCheckHelper.assertIndices({ 'rows': rows, 'cols': cols });
+		let data = (new Array(rows * cols)).fill(value);
+		return new Matrix(rows, cols, data);
 	}
 
-	// TODO: Add test
 	static isMatrix(value) {
 		return value !== null && value instanceof Matrix;
 	}
 
-	// TODO: Add test
 	len() {
 		return this.data.length;
 	}
 
-	// TODO: Add test
 	at(rowIndex, colIndex = undefined) {
-		if (colIndex !== undefined) ErrorCheckHelper.assertIndices({'rowIndex': rowIndex, 'colIndex': colIndex});
-		else ErrorCheckHelper.assertIndices({'rowIndex': rowIndex});
-		let index = (colIndex === undefined) ? rowIndex : this.indexFromRowCol(rowIndex, colIndex);
+		let index;
+		if (colIndex !== undefined) {
+			// ERR: Doesn't allow an index of 0
+			ErrorCheckHelper.assertIndices({ 'rowIndex': rowIndex, 'colIndex': colIndex });
+			if (rowIndex > this.rows) throw new RangeError('rowIndex was out of range');
+			if (colIndex > this.cols) throw new RangeError('colIndex was out of range');
+			index = this.indexFromRowCol(rowIndex, colIndex);
+		}
+		else {
+			// ERR: Doesn't allow an index of 0
+			ErrorCheckHelper.assertIndices({ 'rowIndex': rowIndex });
+			if (rowIndex > this.len()) throw new RangeError('index was out of range');
+			index = rowIndex;
+		}
 		return this.data[index];
 	}
 
@@ -68,39 +74,38 @@ class Matrix {
 	// TODO: Add test
 	mul(other) {
 		if (!Matrix.isMatrix(other)) throw new TypeError('Parameter must be a Matrix');
-        return this.data.map((val, index) => {
+		return this.data.map((val, index) => {
 			// TODO: Implement
-        });
-    }
+		});
+	}
 
 	// TODO: Add test
-    div(other) {
+	div(other) {
 		if (!Matrix.isMatrix(other)) throw new TypeError('Parameter must be a Matrix');
 		// TODO: Implement
-    }
+	}
 
 	// TODO: Add test
 	rowFromIndex(index) {
-		ErrorCheckHelper.assertIndices({'index': index});
+		ErrorCheckHelper.assertIndices({ 'index': index });
 		return Math.floor(index / this.cols);
 	}
 
 	// TODO: Add test
 	colFromIndex(index) {
-		ErrorCheckHelper.assertIndices({'index': index});
+		ErrorCheckHelper.assertIndices({ 'index': index });
 		return index % (this.cols + 1);
 	}
 
 	// TODO: Add test
 	indexFromRowCol(row, col) {
-		ErrorCheckHelper.assertIndices({'rows': rows, 'cols': cols});
+		ErrorCheckHelper.assertIndices({ 'rows': rows, 'cols': cols });
 		return (this.cols * row) + col;
 	}
 
 }
 
 class ErrorCheckHelper {
-	// TODO: Add test
 	static assertIndices(options) {
 		for (let [key, value] of Object.entries(options)) {
 			if (!Number.isInteger(value)) throw new TypeError(`[${key}] must be an integer`);
@@ -110,3 +115,4 @@ class ErrorCheckHelper {
 }
 
 exports.Matrix = Matrix;
+exports.ErrorCheckHelper = ErrorCheckHelper;
--- test/test.js
+++ test/test.js
@@ -1,48 +1,102 @@
 let assert = require('assert');
-let { Matrix } = require('../src/matrix');
+let { Matrix, ErrorCheckHelper } = require('../src/matrix');
 
-describe('Matrix', function() {
-    describe('#constructor()', function() {
-        it('should throw TypeError if rows or cols are not integers greater than 0', function() {
-            assert.throws(() => { new Matrix(2, -2, [1, 2, 3, 4]); }, TypeError);
-            assert.throws(() => { new Matrix(-2, 2, [1, 2, 3, 4]); }, TypeError);
-            assert.throws(() => { new Matrix('22', 2, [1, 2, 3, 4]); }, TypeError);
-            assert.throws(() => { new Matrix(2, '22', [1, 2, 3, 4]); }, TypeError);
-            assert.throws(() => { new Matrix([2], 2, [1, 2, 3, 4]); }, TypeError);
-            assert.throws(() => { new Matrix(2, [2], [1, 2, 3, 4]); }, TypeError);
-        });
-        it('should throw TypeError if data is not an array', function() {
-            assert.throws(() => { new Matrix(2, 2, 1); }, TypeError);
-            assert.throws(() => { new Matrix(2, 2, '11'); }, TypeError);
-        })
-        it('should throw RangeError if rows*cols doesnt equal the length of data', function() {
-            assert.throws(() => { new Matrix(2, 2, [1, 2, 3]); }, RangeError);
-            assert.throws(() => { new Matrix(2, 2, [1, 2, 3, 4, 5]); }, RangeError);
-            assert.throws(() => { new Matrix(1, 5, [1, 2, 3, 4]); }, RangeError);
-        });
-        it('should create a Matrix with the expected values', function() {
-            let m = new Matrix(2, 3, [1, 2, 3, 4, 5, 6]);
-            assert.strictEqual(m.rows, 2);
-            assert.strictEqual(m.cols, 3);
-            assert.deepStrictEqual(m.data, [1, 2, 3, 4, 5, 6]);
-        });
-    });
-    describe('#from2DArray()', function() {
-        it('should throw TypeError if data is not a multidimentional array', function(){
-            assert.throws(() => { Matrix.from2DArray(1); }, TypeError);
-            assert.throws(() => { Matrix.from2DArray('11'); }, TypeError);
-            assert.throws(() => { Matrix.from2DArray([1, 2, 3, 4]); }, TypeError);
-            assert.throws(() => { Matrix.from2DArray(['11', '22']); }, TypeError);
-        });
-        it('should throw RangeError if data is not rectangular', function() {
-            assert.throws(() => { Matrix.from2DArray([[1, 2], [1, 2, 3]]); }, RangeError);
-            assert.throws(() => { Matrix.from2DArray([[1, 2, 3], [1, 2, 3, 4], [1, 2, 3]]); }, RangeError);
-        });
-        it ('should create a Matrix with the expected values', function() {
-            let m = Matrix.from2DArray([[1, 2, 3], [4, 5, 6]]);
-            assert.strictEqual(m.rows, 2);
-            assert.strictEqual(m.cols, 3);
-            assert.deepStrictEqual(m.data, [1, 2, 3, 4, 5, 6]);
-        });
-    });
+describe('Matrix', function () {
+	describe('#constructor()', function () {
+		it('should throw TypeError if data is not an array', function () {
+			assert.throws(() => { new Matrix(2, 2, 1); }, TypeError);
+			assert.throws(() => { new Matrix(2, 2, '11'); }, TypeError);
+		})
+		it('should throw RangeError if rows*cols doesnt equal the length of data', function () {
+			assert.throws(() => { new Matrix(2, 2, [1, 2, 3]); }, RangeError);
+			assert.throws(() => { new Matrix(2, 2, [1, 2, 3, 4, 5]); }, RangeError);
+			assert.throws(() => { new Matrix(1, 5, [1, 2, 3, 4]); }, RangeError);
+		});
+		it('should create a Matrix with the expected values', function () {
+			let m = new Matrix(2, 3, [1, 2, 3, 4, 5, 6]);
+			assert.strictEqual(m.rows, 2);
+			assert.strictEqual(m.cols, 3);
+			assert.deepStrictEqual(m.data, [1, 2, 3, 4, 5, 6]);
+		});
+	});
+	describe('#from2DArray()', function () {
+		it('should throw TypeError if data is not a multidimentional array', function () {
+			assert.throws(() => { Matrix.from2DArray(1); }, TypeError);
+			assert.throws(() => { Matrix.from2DArray('11'); }, TypeError);
+			assert.throws(() => { Matrix.from2DArray([1, 2, 3, 4]); }, TypeError);
+			assert.throws(() => { Matrix.from2DArray(['11', '22']); }, TypeError);
+		});
+		it('should throw RangeError if data is not rectangular', function () {
+			assert.throws(() => { Matrix.from2DArray([[1, 2], [1, 2, 3]]); }, RangeError);
+			assert.throws(() => { Matrix.from2DArray([[1, 2, 3], [1, 2, 3, 4], [1, 2, 3]]); }, RangeError);
+		});
+		it('should create a Matrix with the expected values', function () {
+			let m = Matrix.from2DArray([[1, 2, 3], [4, 5, 6]]);
+			assert.strictEqual(m.rows, 2);
+			assert.strictEqual(m.cols, 3);
+			assert.deepStrictEqual(m.data, [1, 2, 3, 4, 5, 6]);
+		});
+	});
+	describe('#filled()', function () {
+		it('should create a Matrix with the given dimensions', function () {
+			let m = Matrix.filled(2, 3, 0);
+			assert.strictEqual(m.rows, 2);
+			assert.strictEqual(m.cols, 3);
+		});
+		it('should create a Matrix filled with the given value', function () {
+			let m = Matrix.filled(2, 3, 0);
+			assert.deepStrictEqual(m.data, [0, 0, 0, 0, 0, 0]);
+		});
+	});
+	describe('#isMatrix()', function () {
+		it('should only return true if passed a Matrix', function () {
+			assert.strictEqual(Matrix.isMatrix(Matrix.ones(2, 3)), true);
+			assert.strictEqual(Matrix.isMatrix(undefined), false);
+			assert.strictEqual(Matrix.isMatrix(null), false);
+			assert.strictEqual(Matrix.isMatrix(1), false);
+			assert.strictEqual(Matrix.isMatrix(Matrix), false);
+		});
+	});
+	describe('#len()', function () {
+		it('should return the correct length', function () {
+			assert.strictEqual(Matrix.zeros(2, 3).len(), 6);
+			assert.strictEqual(Matrix.zeros(10, 20).len(), 200);
+		});
+	});
+	describe('#at()', function () {
+		it('should throw a RangeError if index is out of range', function() {
+			let m = Matrix.from2DArray([[1, 2, 3], [4, 5, 6]]);
+			assert.throws(() => { m.at(7) }, RangeError);
+			assert.throws(() => { m.at(0, 4) }, RangeError);
+			assert.throws(() => { m.at(3, 1) }, RangeError);
+		});
+		it('should return the correct value when given 1 parameter', function () {
+			let m = Matrix.from2DArray([[1, 2, 3], [4, 5, 6]]);
+			assert.strictEqual(m.at(0), m.data[0]);
+			assert.strictEqual(m.at(0), 1);
+			assert.strictEqual(m.at(4), 5);
+		});
+		it('should return the correct value when given 2 parameters', function () {
+			let m = Matrix.from2DArray([[1, 2, 3], [4, 5, 6]]);
+			assert.strictEqual(m.at(0, 0), m.data[0]);
+			assert.strictEqual(m.at(0, 0), 1);
+			assert.strictEqual(m.at(1, 2), 6);
+		});
+	});
+});
+
+describe('ErrorCheckHelper', function () {
+	describe('#assertIndices()', function () {
+		it('should throw a TypeError if any of the values are not an Integer', function () {
+			assert.throws(() => { ErrorCheckHelper.assertIndices({ 'a': 'a' }) }, TypeError);
+			assert.throws(() => { ErrorCheckHelper.assertIndices({ 'a': [1] }) }, TypeError);
+			assert.throws(() => { ErrorCheckHelper.assertIndices({ 'a': 1, 'b': 2, 'c': '3' }) }, TypeError);
+			assert.throws(() => { ErrorCheckHelper.assertIndices({ 'a': 1.2 }) }, TypeError);
+		});
+		it('should throw a RangeError if any of the values are 0 or less', function () {
+			assert.throws(() => { ErrorCheckHelper.assertIndices({ 'a': 0 }) }, RangeError);
+			assert.throws(() => { ErrorCheckHelper.assertIndices({ 'a': -2 }) }, RangeError);
+			assert.throws(() => { ErrorCheckHelper.assertIndices({ 'a': 2, 'b': 3, 'c': 0 }) }, RangeError);
+		});
+	});
 });
